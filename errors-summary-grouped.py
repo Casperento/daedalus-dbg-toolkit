d21f18f9da68d7e8345cb8a70121c6f36c4c2343
Casperento <44746868+Casperento@users.noreply.github.com>
2025-05-23 22:35:16 -0300

    new error pattern

--- errors-summary-grouped.py
+++ errors-summary-grouped.py
@@ -17,6 +17,7 @@ def parse_errors(file_path):
         r"Basic Block in function '(.+)' does not have terminator!",
         r"Only PHI nodes may reference their own value!",
         r"Assertion\s`(.+)\sfailed\.",
+        r"Referring to an argument in another function!"
     ]
     patterns = [re.compile(p) for p in raw_patterns]
 
