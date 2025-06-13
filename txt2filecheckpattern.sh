dc89163d19346ab9e5d22ae0e3241540307fe6f1
Casperento <44746868+Casperento@users.noreply.github.com>
2025-06-13 17:58:47 -0300

    new script for patterns

---
+++ txt2filecheckpattern.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# filepath: /home/reckstein/src/github/errors-dbg-framework/txt2filecheckpattern.sh
+
+# Parse arguments using getopt
+usage() {
+    echo "Usage: $0 -f, --file <inputfile>"
+    exit 1
+}
+
+OPTS=$(getopt -o f:h --long file:,help -n 'parse-options' -- "$@")
+if [ $? != 0 ]; then usage; fi
+
+eval set -- "$OPTS"
+
+INPUTFILE=""
+while true; do
+  case "$1" in
+    -f | --file ) INPUTFILE="$2"; shift 2 ;;
+    -h | --help ) usage ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [ -z "$INPUTFILE" ]; then
+    usage
+fi
+
+# Apply sed transformations to the input file
+sed -i '1s/^/; CHECK: /' "$INPUTFILE"
+sed -i 's/^/; CHECK-NEXT: /g' "$INPUTFILE"
+sed -i 's/^; CHECK-NEXT: $/;CHECK-EMPTY:/g' "$INPUTFILE"
+sed -i '1s/; CHECK-NEXT: //g' "$INPUTFILE"
