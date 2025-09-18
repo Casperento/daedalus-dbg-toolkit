ff2b4acfa23607c458c4abbabf8443b3ac0fb2aa
Casperento <44746868+Casperento@users.noreply.github.com>
2025-09-18 15:13:55 -0300

    change pattern replacement exp

--- txt2filecheckpattern.sh
+++ txt2filecheckpattern.sh
@@ -3,8 +3,8 @@
 
 # Parse arguments using getopt
 usage() {
-    echo "Usage: $0 -f, --file <inputfile>"
-    exit 1
+  echo "Usage: $0 -f, --file <inputfile>"
+  exit 1
 }
 
 OPTS=$(getopt -o f:h --long file:,help -n 'parse-options' -- "$@")
@@ -15,15 +15,21 @@ eval set -- "$OPTS"
 INPUTFILE=""
 while true; do
   case "$1" in
-    -f | --file ) INPUTFILE="$2"; shift 2 ;;
-    -h | --help ) usage ;;
-    -- ) shift; break ;;
-    * ) break ;;
+  -f | --file)
+    INPUTFILE="$2"
+    shift 2
+    ;;
+  -h | --help) usage ;;
+  --)
+    shift
+    break
+    ;;
+  *) break ;;
   esac
 done
 
 if [ -z "$INPUTFILE" ]; then
-    usage
+  usage
 fi
 
 # Apply sed transformations to the input file
@@ -31,4 +37,4 @@ sed -i '1s/^/; CHECK: /' "$INPUTFILE"
 sed -i 's/^/; CHECK-NEXT: /g' "$INPUTFILE"
 sed -i 's/^; CHECK-NEXT: $/; CHECK-EMPTY:/g' "$INPUTFILE"
 sed -i '1s/; CHECK-NEXT: //g' "$INPUTFILE"
-sed -i 's/__[0-9]\+(/__[[ID:[0-9]+]](/g' "$INPUTFILE"
+sed -i 's/__[0-9]\+(/_[[ID:[0-9]+]](/g' "$INPUTFILE"
